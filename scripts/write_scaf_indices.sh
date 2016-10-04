4dca9ca1c0dec7d460fd6c15a139f33630585087
willpitchers <willpitchers@gmail.com>
2016-10-04 17:11:49 -0400

    fixed scaf indexing script

--- scripts/write_scaf_indices.sh
+++ scripts/write_scaf_indices.sh
@@ -10,10 +10,11 @@ for i in `seq 1 4667`  ; do
 	if [ ${thisLen} -gt ${chunkLen} ] ; then
 		theseBreaks=( `seq 1 ${chunkLen} ${thisLen}` )
 		nBreaks=`expr ${#theseBreaks[@]} - 1`
-		for j in `seq 0 ${nBreaks}` ; do
+		for j in $(seq 0 $(expr ${nBreaks} - 1) ) ; do
 			echo ${thisScaf}:${theseBreaks[${j}]}-${theseBreaks[${j}+1]}
 		done
-	fi	
+		echo ${thisScaf}:${theseBreaks[-1]}-${thisLen}
+	fi
 	if [ ${thisLen} -lt `expr ${chunkLen} / 8` ] ; then
         echo ${thisScaf} 8th
     elif [ ${thisLen} -lt `expr ${chunkLen} / 2` ] ; then
