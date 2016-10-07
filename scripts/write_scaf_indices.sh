86c27aef072433ada931e8a1b294e84eef0c671d
willpitchers <willpitchers@gmail.com>
2016-10-07 13:52:47 -0400

    updated index generating script

--- scripts/write_scaf_indices.sh
+++ scripts/write_scaf_indices.sh
@@ -14,22 +14,32 @@ for i in `seq 1 4667`  ; do
 			echo ${thisScaf}:${theseBreaks[${j}]}-${theseBreaks[${j}+1]}
 		done
 		echo ${thisScaf}:${theseBreaks[@]:(-1)}-${thisLen}
-	fi
-	if [ ${thisLen} -lt `expr ${chunkLen} / 8` ] ; then
-        echo ${thisScaf} 8th
-    elif [ ${thisLen} -lt `expr ${chunkLen} / 2` ] ; then
-        echo ${thisScaf} half
+#	fi
+#	if [ ${thisLen} -lt `expr ${chunkLen} / 8` ] ; then
+#        echo ${thisScaf} 8th
+#    elif [ ${thisLen} -lt `expr ${chunkLen} / 2` ] ; then
+#        echo ${thisScaf} half
 	elif [ ${thisLen} -lt ${chunkLen} ] ; then
         echo ${thisScaf}
     fi
 done > indices.list
 
-sed -i s/\-$//g indices.list
+#sed -i s/\-$//g indices.list
 
-sed -i '/half/{N;s/\n//;}' indices.list
-sed -i s/half//g indices.list
+#sed -i '/half/{N;s/\n//;}' indices.list
+#sed -i s/half//g indices.list
 
-for i in `seq 1 3` ; do
-	sed -i '/8th/{N;s/\n//;}' indices.list
+#for i in `seq 1 3` ; do
+#	sed -i '/8th/{N;s/\n//;}' indices.list
+#done
+#sed -i s/8th//g indices.list
+
+rm indices.long.list
+touch indices.long.list
+
+for i in `cat indices.list` 
+do for j in `seq 1 63` 
+  do ind=`head -${j} samples.list | tail -1` 
+  echo ${ind} ${i} >> indices.long.list 
+ done 
 done
-sed -i s/8th//g indices.list
