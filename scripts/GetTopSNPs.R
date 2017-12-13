ebd78fb9ae1d5168559cc4c65a9219fd69bc9935
willpitchers <willpitchers@gmail.com>
2017-12-13 11:50:52 -0500

    just merge dammit

--- scripts/GetTopSNPs.R
+++ scripts/GetTopSNPs.R
@@ -12,23 +12,23 @@ filename <- args[ 1 ]
 assoc <- tbl_df( fread( filename,  header=TRUE ))
 
 if ( grepl( "model", filename ) == TRUE ) {
-		assoc %>% arrange( P ) %>% mutate( BP=as.numeric( gsub( "Var-Scaffold[0-9]+-", "", SNP ))) %>% head( 1000 ) %>%
-				write.csv( paste( filename, coll='', sep='' ), quote=FALSE, row.names=FALSE )
+		assoc %>% arrange( P ) %>% mutate( BP=as.numeric( gsub( "Var-Scaffold[0-9]+-", "", SNP ))) %>% head( 10000 ) %>%
+				write.csv( paste( filename, ".topsnps.csv", coll='', sep='' ), quote=FALSE, row.names=FALSE )
 	}
 
 
-if ( grepl( "assoc.logistic", filename ) == TRUE ) {
-		perms <- tbl_df( fread( paste( filename, ".mperm", sep='', coll='' )))
-		full_join( assoc, perms, by=c( "SNP", "SNP" )) %>% 
-		arrange( EMP2 ) %>%
+#if ( grepl( "assoc.logistic", filename ) == TRUE ) {
+#		perms <- tbl_df( fread( paste( filename, ".mperm", sep='', coll='' )))
+#		full_join( assoc, perms, by=c( "SNP", "SNP" )) %>% 
+#		arrange( EMP2 ) %>%
 #		arrange( P ) %>%
-				head( 5000 ) %>% write.csv( paste( filename, ".csv", coll='', sep='' ), quote=FALSE, row.names=FALSE )
-	}
+#				head( 5000 ) %>% write.csv( paste( filename, ".csv", coll='', sep='' ), quote=FALSE, row.names=FALSE )
+#	}
 
 
 if ( grepl( "fisher", filename ) == TRUE ) {
 		assoc <- transmute( assoc, Scaf=factor( CHR ), SNP=BP, A1=factor( A1 ), F_A=F_A, F_U=F_U, A2=factor( A2 ), P=P, OR=OR, SE=SE )
-		assoc %>% arrange( P ) %>% head( 1000 ) %>% write.csv( paste( filename, ".csv", coll='', sep='' ), quote=FALSE, row.names=FALSE )
+		assoc %>% arrange( P ) %>% head( 10000 ) %>% write.csv( paste( filename, ".topsnps.csv", coll='', sep='' ), quote=FALSE, row.names=FALSE )
 	}
 
 q( 'no' )
